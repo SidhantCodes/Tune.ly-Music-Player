226c2c95e39b7716022eea8f65461f246636b619
Sidhant Mishra <mishrasidhant01@gmail.com>
2023-09-24 16:19:52 +0530

    Fixed play/pause icon bug and updated transitions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let songIndex = 0;
 let audioElement = new Audio('Audio/AfterHours.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let nowPlaying = document.getElementById('nowPlaying');
 let songs = [
     {songName: "After Hours", filePath: "Audio/AfterHours.mp3", coverPath: "pictures/cover2.jpg", artist: "The Weeknd"},
     {songName: "After Hours", filePath: "Audio/AfterHours.mp3", coverPath: "pictures/cover2.jpg", artist: "The Weeknd"},
@@ -14,17 +16,20 @@ let songs = [
     {songName: "After Hours", filePath: "Audio/AfterHours.mp3", coverPath: "pictures/cover2.jpg", artist: "The Weeknd"}
 ]
 
-masterPlay.addEventListener('click', ()=>{
-    if(audioElement.paused || audioElement.currentTime <=0){
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
-        masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-circle-pause');
-    }else{
+        masterPlay.innerHTML = "&#xf04c;";
+        gif.style.opacity = 1; 
+        nowPlaying.style.opacity = 1
+    } else {
         audioElement.pause();
-        masterPlay.classList.remove('fa-circle-pause');
-        masterPlay.classList.add('fa-play-circle');
+        masterPlay.innerHTML = "&#xf04b;"; 
+        gif.style.opacity = 0; 
+        nowPlaying.style.opacity = 0
     }
 });
+
 // audioElement.play();
 myProgressBar.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
